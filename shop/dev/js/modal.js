688b37284aa2fd5dea5d07f96eabed5d227ecf5f
maisusdecerulinstelat <124612781+maisusdecerulinstelat@users.noreply.github.com>
2023-07-12 11:33:47 +0300

    websitebuilding

---
+++ shop/dev/js/modal.js
@@ -0,0 +1,24 @@
+
+var id;
+$("#modalForm").on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget); //Button that triggered the modal
+    const produs = button.data('name') //need to find the button and get product
+    const pret = button.data('price') //need to find the button and get price
+    const description = button.data('description');
+    $(".modal-header #titleModalLabel").html(produs);
+    $(".modal-body #price").html(pret);
+    $(".modal-body #description").html(description);
+    id = button.attr('id');
+    // var datainfo = document.getElementById(id);
+     alert(JSON.stringify(button[0]));
+    // alert(JSON.stringify(datainfo));
+    
+   
+  })
+  $("#modalForm").on('hidden.bs.modal', function(event) {
+   
+    alert('Modalul s-a inchis');
+    document.getElementById(id).classList.remove('btn-primary');
+    document.getElementById(id).classList.add('btn-success');
+  })
+  
